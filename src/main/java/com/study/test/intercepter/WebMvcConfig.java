e6f17dd64e2110a07255497380f610a13450df71
Leedius <tiuy2750@gmail.com>
2023-05-16 14:58:58 +0900

    login_page update

--- src/main/java/com/study/test/intercepter/WebMvcConfig.java
+++ src/main/java/com/study/test/intercepter/WebMvcConfig.java
@@ -2,20 +2,46 @@ package com.study.test.intercepter;
 
 import java.util.Locale;
 
-import org.springframework.context.MessageSource;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class WebMvcConfig implements WebMvcConfigurer {
-    @Bean
-    public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.KOREAN);
+	
+	@Bean
+	public ReloadableResourceBundleMessageSource messageSource() {
+		ReloadableResourceBundleMessageSource source = new ReloadableResourceBundleMessageSource();
+
+		// 메세지 프로퍼티파일의 위치와 이름을 지정한다.
+		source.setBasename("classpath:/i18n/messages");
+
+		// 기본 인코딩을 지정한다.
+		source.setDefaultEncoding("UTF-8");
+
+		// 프로퍼티 파일의 변경을 감지할 시간 간격을 지정한다.
+		source.setCacheSeconds(60);
+
+		// 없는 메세지일 경우 예외를 발생시키는 대신 코드를 기본 메세지로 한다.
+		source.setUseCodeAsDefaultMessage(true);
+
+		return source;
+
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(new Locale("ko"));
         return slr;
     }
 
@@ -32,12 +58,4 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		registry.addInterceptor(localeChangeInterceptor());
 	}
 	
-	@Bean
-	public MessageSource messageSource() {
-	    ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-	    messageSource.setBasename("classpath:/i18n/messages");
-	    messageSource.setDefaultEncoding("UTF-8");
-	    return messageSource;
-	}
-
 }
