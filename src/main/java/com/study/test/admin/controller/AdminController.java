000038da6f7429049d90a1da5202dcfe02b1160e
jongjin8976 <whdwls8976@gmail.com>
2023-05-16 14:52:28 +0900

    transaction

--- src/main/java/com/study/test/admin/controller/AdminController.java
+++ src/main/java/com/study/test/admin/controller/AdminController.java
@@ -1,19 +1,19 @@
 package com.study.test.admin.controller;
 
-import java.io.File;
-import java.util.UUID;
+
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.multipart.MultipartFile;
+
 
 import com.study.test.admin.service.AdminService;
 import com.study.test.member.service.MemberService;
 import com.study.test.member.vo.MemImgVO;
 import com.study.test.member.vo.MemberVO;
-import com.study.test.util.ConstVariable;
 import com.study.test.util.UploadUtil;
 
 import jakarta.annotation.Resource;
@@ -38,27 +38,20 @@ public class AdminController {
 	//회원등록
 	@PostMapping("/join")
 	public String join(MemberVO memberVO, MultipartFile mainImg) {
-		System.out.println("@@@@@@@@@@@@@@@@@@@@@@" + mainImg);
-		System.out.println(memberVO);
-		String originFileName = mainImg.getOriginalFilename();
-        String uuid = UUID.randomUUID().toString();
-        String extension = originFileName.substring(originFileName.lastIndexOf("."));
-        //첨부될 파일명
-        String attachedFileName = uuid + extension;
-        
-        //파일 업로드
-        
-        
-        try {
-            File file = new File(ConstVariable.UPLOAD_PATH + attachedFileName);
-            mainImg.transferTo(file);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-		
-		//memberService.regMember(memberVO);
+		//UploadUtill 객체 호출해서(util패키지에 만들어놓음)MemImgVO 객체에 받음
+		MemImgVO attachedImgVO = UploadUtil.uploadFile(mainImg);
+		//memberVO에서 받아온 memNo memImgVO에 넣음
+		attachedImgVO.setMemNo(memberVO.getMemNo());
+		//다음에 들어갈 img코드 호출해서 세팅
+		attachedImgVO.setImgCode(memberService.getNextImgCode());
+		//memberVO.memImage에 imgCode 세팅
+		memberVO.setMemImage(attachedImgVO.getImgCode());
+		//memberVO안에있는 memImgVO에 UploadUtill로 불러온 데이터 넣음(트랜잭션처리때문에)
+		memberVO.setMemImgVO(attachedImgVO);
+		System.out.println("@@@@@@@@@@@@@@"+ memberVO);
+		memberService.regMember(memberVO);
 		
 		
-		return "redirect:/";
+		return "redirect:/admin/join";
 	}
 }
--- src/main/java/com/study/test/member/service/MemberService.java
+++ src/main/java/com/study/test/member/service/MemberService.java
@@ -2,6 +2,7 @@ package com.study.test.member.service;
 
 import java.util.List;
 
+
 import com.study.test.member.vo.MemberMenuVO;
 import com.study.test.member.vo.MemberVO;
 
@@ -19,4 +20,6 @@ public interface MemberService {
 	//로그인
 	MemberVO login(MemberVO memberVO);
 	
+	//다음에 들어가는 IMG코드 조회
+	String getNextImgCode();
 }
