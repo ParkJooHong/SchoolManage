b437125763b8cdcbcb880786affad2d3049e558f
Leedius <tiuy2750@gmail.com>
2023-06-12 15:05:41 +0900

    바텀 업데이트

--- src/main/resources/static/js/fragment/info_side.js
+++ src/main/resources/static/js/fragment/info_side.js
@@ -3,8 +3,6 @@ function mainMenu(menuCode, menuUrl){
 	location.href=`/stuMenu${menuUrl}?menuCode=${menuCode}`;
 
 	const change = document.querySelector('.submenuTd');
-	
-	change.style.backgroundColor = "#870119";
 }
 
 function subMenu(subMenuCode, subMenuUrl, menuCode){
