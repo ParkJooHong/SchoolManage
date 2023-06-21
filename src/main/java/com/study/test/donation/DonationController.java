80c47ad83274c6ece4c51bce35cd909fad365795
Fast2182 <fast8821@gmail.com>
2023-06-21 12:27:03 +0900

    "UsedMarektCommit"

--- src/main/java/com/study/test/donation/DonationController.java
+++ src/main/java/com/study/test/donation/DonationController.java
@@ -1,7 +1,5 @@
 package com.study.test.donation;
 
-
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,44 +14,38 @@ import jakarta.annotation.Resource;
 @Controller
 public class DonationController {
 
-    @Resource(name = "donationService")
-    private DonationService donationService;
+	@Resource(name = "donationService")
+	private DonationService donationService;
 
-    
-     //중고물품 등록
+	// 중고물품 등록
 	@PostMapping("/regDonation")
-	public String donationInsert(DonationBoardVO donationBoardVO ,
-	     MultipartFile mainImg) {
-		
+	public String donationInsert(DonationBoardVO donationBoardVO,
+			MultipartFile mainImg) {
 
 		// UploadUtill 객체 호출해서(util패키지에 만들어놓음)
-		//DonationBoardImageVO객체를 attachedImgVO 변수에
-		//할당한 후 에 이후 attachedImgVO 객체를 활용하여 필요한 작업 수행
-		
-		//DoatnionBoardImageVO 객체에서 아예 받아버림
-	
-		DonationBoardImageVO attachedImgVO = UploadUtil.uploadDonationFile(mainImg);
-		
-		attachedImgVO.setDonationCode(donationBoardVO.getDonationCode());
-		// 다음에 들어갈 img코드 호출해서 세팅하는 메소드 
-		attachedImgVO.setImgCode(donationService.getNextDonationImgCode());
-		
+		// DonationBoardImageVO객체를 attachedImgVO 변수에
+		// 할당한 후 에 이후 attachedImgVO 객체를 활용하여 필요한 작업 수행
 
-		// donationBoard VO에 있는 DonationImage에 attachedimgVO 
-		//즉 위에서 만든 attachedImgVO = 자체가 
+		// DoatnionBoardImageVO 객체에서 아예 받아버림
+
+		DonationBoardImageVO attachedImgVO = UploadUtil.uploadDonationFile(mainImg);
+
+		attachedImgVO.setDonationCode(donationBoardVO.getDonationCode());
+		// 다음에 들어갈 img코드 호출해서 세팅하는 메소드
+		attachedImgVO.setImgCode(donationService.getNextDonationImgCode());
+
+		// donationBoard VO에 있는 DonationImage에 attachedimgVO
+		// 즉 위에서 만든 attachedImgVO = 자체가
 		donationBoardVO.setDonationImage(attachedImgVO.getImgCode());
 
 		// memberVO안에있는 memImgVO에 UploadUtill로 불러온 데이터 넣음
-		//트랜 잭션 처리 때문에 
+		// 트랜 잭션 처리 때문에
 		donationBoardVO.setDonationBoardImageVO(attachedImgVO);
 
 		donationService.insertDonation(donationBoardVO);
-        
-		
+
 		return "redirect:/stuMenu/donation";
 
 	}
 
 }
-
-
--- src/main/java/com/study/test/donation/DonationService.java
+++ src/main/java/com/study/test/donation/DonationService.java
@@ -7,12 +7,11 @@ public interface DonationService {
     // 중고 상품을 데이터베이스에 등록하는 쿼리문
     void insertDonation(DonationBoardVO donationBoardVO);
 
-    //다음 이미지 코드를 조회하는 코드 
+    // 다음 이미지 코드를 조회하는 코드
     String getNextDonationImgCode();
 
     // 중고 상품을 데이터베이스에서 조회하는 쿼리문
     List<DonationBoardVO> donationSearch();
-
 
     // 중고 상품 상세보기 조회하는 쿼리문
     DonationBoardVO detailDonation(String donationCode);
